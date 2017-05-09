bfb34c81143f4dc14112630a00ffa6255eef8760
Zoran <zoran.heinczinger@gmail.com>
2017-05-09 23:07:24 +0200

    Library hack

--- new_site/config/app.php
+++ new_site/config/app.php
@@ -167,7 +167,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
-	Fish\Laratabs\LaratabsServiceProvider::class,
+	    Fish\Laratabs\LaratabsServiceProvider::class,
 
         /*
          * Application Service Providers...
