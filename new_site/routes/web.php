783be6b953af785c2c8129b7cc0aa425bfa8ec98
Zoran <zoran.heinczinger@gmail.com>
2017-05-12 22:10:32 +0200

    Modify web routes

--- new_site/routes/web.php
+++ new_site/routes/web.php
@@ -1,7 +1,10 @@
 <?php
+use App\konyv;
+use App\Miserend;
+use Illuminate\Support\Facades\Input;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home')->withDetails ( Miserend::all() );
 });
 
 Route::get('/admin', function () {
@@ -9,45 +12,45 @@ Route::get('/admin', function () {
 });
 
 Route::get('/romzsa_teodor', function () {
-    return view('romzsa_teodor');
+    return view('szemelyek.romzsa_teodor');
 });
 
 Route::get('/seres_ferenc', function () {
-    return view('seres_ferenc');
+    return view('szemelyek.seres_ferenc');
 });
 
 Route::get('/mihalovics_zsigmond', function () {
-    return view('mihalovics_zsigmond');
+    return view('szemelyek.mihalovics_zsigmond');
 });
 
 // Rólunk //
 
 Route::get('/plebania', function () {
-    return View::make('plebania', ['tabs'=>Tabs::get('kassaiter')]);
+    return View::make('rolunk.plebania', ['tabs'=>Tabs::get('kassaiter')]);
 });
 
 Route::get('/szolgalat', function () {
-    return View::make('szolgalat', ['tabs'=>Tabs::get('szentsegek')]);
+    return View::make('rolunk.szolgalat', ['tabs'=>Tabs::get('szentsegek')]);
 });
 
 Route::get('/kollegium', function () {
-    return view('kollegium');
+    return view('rolunk.kollegium');
 });
 
 Route::get('/lelkipasztoraink', function () {
-    return view('lelkipasztoraink');
+    return view('rolunk.lelkipasztoraink');
 });
 
 Route::get('/egyhaztanacs', function () {
-    return view('egyhaztanacs');
+    return view('rolunk.egyhaztanacs');
 });
 
 Route::get('/kozossegek', function () {
-    return View::make('kozossegek', ['tabs'=>Tabs::get('kozossegek')]);
+    return View::make('rolunk.kozossegek', ['tabs'=>Tabs::get('kozossegek')]);
 });
 
 Route::get('/elerhetoseg', function () {
-    return view('elerhetoseg');
+    return view('rolunk.elerhetoseg');
 });
 
 // Főmenü //
@@ -72,6 +75,16 @@ Route::get('/ad_keres', function () {
     return view('ad_keres');
 });
 
-Route::get('/konyvtar', function () {
-    return view('konyvtar');
-});
+Route::get ( '/konyvtar', function () {
+    return view ( 'konyvtar' )->withDetails ( konyv::all() );
+} );
+
+Route::any ( '/konyvtar_kereso', function () {
+    $q = Input::get ( 'q' );
+    $cnt = 0;
+    $book = konyv::where ( 'cim', 'LIKE', '%' . $q . '%' )->orWhere ( 'szerzo', 'LIKE', '%' . $q . '%' )->get ();
+    if (count ( $book ) > 0)
+        return view ( 'konyvtar' )->withDetails ( $book )->withQuery ( $q )->withCounter ( $cnt );
+    else
+        return view ( 'konyvtar' );
+} );
