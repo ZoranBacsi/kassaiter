2830e3c37fc2cbb1c92ee2026e50fae15211920d
Zoran <rendszergazda.kassaiter@gmail.com>
2017-06-05 18:15:03 +0200

    Finalize auth module

--- new_site/resources/views/components/nav.blade.php
+++ new_site/resources/views/components/nav.blade.php
@@ -51,15 +51,19 @@
                 <li>
                     <a href="/konyvtar">Könyvtár</a>
                 </li>
+            </ul>
+            <ul class="nav navbar-nav pull-right">
                 @if (Auth::guest())
                     <li><a href="{{ route('login') }}">Bejelentkezés</a></li>
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
+                            {{ Auth::user()->name }} <i class="glyphicon glyphicon-chevron-down"></i></a>
                         </a>
-
-                        <ul class="dropdown-menu" role="menu">
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="/register">Felhasználó hozzáadása</a>
+                            </li>
                             <li>
                                 <a href="/admin">Adminisztráció</a>
                             </li>
