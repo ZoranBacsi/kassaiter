70dd45ab641d926b948117f591ef54f067f37efe
Zoran <rendszergazda.kassaiter@gmail.com>
2017-06-05 17:58:05 +0200

    auth module step 1

--- new_site/resources/views/components/nav.blade.php
+++ new_site/resources/views/components/nav.blade.php
@@ -51,6 +51,30 @@
                 <li>
                     <a href="/konyvtar">Könyvtár</a>
                 </li>
+                @if (Auth::guest())
+                    <li><a href="{{ route('login') }}">Bejelentkezés</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="/admin">Adminisztráció</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    Kijelentkezés
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endif
             </ul>
         </div>
     </div>
